ce971ec4cf1a57b37ad9606d8c30beda09eb6c00
RuSlan0605 <phantomdrug06@gmail.com>
2023-11-17 12:19:19 +0500

    1-etap

--- mektep/models.py
+++ mektep/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractUser
 
-# Create your models here.
+class Post(models.Model):
+
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    image = models.ImageField(upload_to='images/%Y/%m/%d')
+    data = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.title
+    
+class Comment(models.Model):
+    pass
